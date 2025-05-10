bf2ccdd5f307811c5427f0a9afa174e73e3bae73
bagoesrex <bagoesrex@gmail.com>
2025-05-10 19:25:46 +0700

    feat: wrap myapp with providerscope

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:placekeeper_app/screens/places.dart';
 
 final colorScheme = ColorScheme.fromSeed(
@@ -19,7 +21,7 @@ final theme = ThemeData().copyWith(
 );
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
