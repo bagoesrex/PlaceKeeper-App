af8e47543fe32e581aa06562e3d4a6f88b250e42
bagoesrex <bagoesrex@gmail.com>
2025-05-11 20:31:56 +0700

    feat: add placedetailscreen and navigate when place on tap

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:placekeeper_app/models/place.dart';
+import 'package:placekeeper_app/screens/place_detail.dart';
 
 class PlacesList extends StatelessWidget {
   const PlacesList({super.key, required this.places});
@@ -28,6 +29,13 @@ class PlacesList extends StatelessWidget {
                 color: Theme.of(context).colorScheme.onSurface,
               ),
             ),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => PlaceDetailScreen(place: places[index]),
+                ),
+              );
+            },
           ),
     );
   }
