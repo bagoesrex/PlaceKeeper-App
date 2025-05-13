45175b9eccc65a6d2b0474475a2f761b1f4ff7ae
bagoesrex <bagoesrex@gmail.com>
2025-05-13 21:21:14 +0700

    feat: display location address and static map in placedetail

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -7,6 +7,12 @@ class PlaceDetailScreen extends StatelessWidget {
 
   final Place place;
 
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+    return 'https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=600&height=300&center=lonlat:$lng,$lat&zoom=15&marker=lonlat:$lng,$lat;type:material;color:%23ff3421;icontype:awesome&scaleFactor=2&apiKey=6b81d0217a2a48a78ddb63ab73d1377f';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +24,40 @@ class PlaceDetailScreen extends StatelessWidget {
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
+          ),
+          Positioned(
+            right: 0,
+            left: 0,
+            bottom: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 65,
+                  backgroundImage: NetworkImage(locationImage),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 16,
+                  ),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.transparent, Colors.black45],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
