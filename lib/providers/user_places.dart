dfca8253208e857b806572ea06894b26b673b1c8
bagoesrex <bagoesrex@gmail.com>
2025-05-17 19:34:41 +0700

    feat: load saved places from sqlite database

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -4,11 +4,48 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite_common/sqlite_api.dart';
 
 import 'package:placekeeper_app/models/place.dart';
 
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)',
+      );
+    },
+    version: 1,
+  );
+  return db;
+}
+
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
+
+  Future<void> loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places =
+        data
+            .map(
+              (row) => Place(
+                id: row['id'] as String,
+                title: row['title'] as String,
+                image: File(row['image'] as String),
+                location: PlaceLocation(
+                  latitude: row['latitude'] as double,
+                  longitude: row['longitude'] as double,
+                  address: row['address'] as String,
+                ),
+              ),
+            )
+            .toList();
+
+    state = places;
+  }
 
   void addPlace(String title, File image, PlaceLocation location) async {
     final appDirectory = await syspaths.getApplicationDocumentsDirectory();
@@ -21,16 +58,7 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
       location: location,
     );
 
-    final dbPath = await sql.getDatabasesPath();
-    final db = await sql.openDatabase(
-      path.join(dbPath, 'places.db'),
-      onCreate: (db, version) {
-        return db.execute(
-          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)',
-        );
-      },
-      version: 1,
-    );
+    final db = await _getDatabase();
     db.insert('user_places', {
       'id': newPlace.id,
       'title': newPlace.title,
